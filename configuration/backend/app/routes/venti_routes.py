3279e99a9835650500055957e1bc7e24e7c65baf
jokley <reinhard_juen@vol.at>
2026-04-21 21:37:03 +0200

    fix: clean up code formatting and improve logging for command handling

--- configuration/backend/app/routes/venti_routes.py
+++ configuration/backend/app/routes/venti_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from datetime import datetime
-from  ..services.venti_service import venti_cmd, venti_auto, venti_auto_param
+from ..services.venti_service import venti_cmd, venti_auto, venti_auto_param
 from ..controller.venti.controller import venti_control
 from ..extensions.extensions import scheduler
 from ..utils.logger import logger
@@ -20,22 +20,28 @@ def switch():
         logger.info('****************************************')
         logger.info('Lüfter Hand ein')
         return jsonify('Venti on')
+    
     elif CMD == 'off':
         venti_cmd(CMD)
         venti_auto(CMD, TM, '0')
         logger.info('****************************************')
         logger.info('Lüfter Hand aus')
         return jsonify('Venti off')
+    
     elif CMD == 'auto':
         venti_auto(CMD, TM, STOCK)
-        # venti_control()
-        scheduler.modify_job('venti_control',next_run_time=datetime.now())
+        # Force scheduler to run now (don't wait 4 minutes)
+        scheduler.modify_job('venti_control', next_run_time=datetime.now())
+        logger.info('Automatik aktiviert')
         return jsonify('Venti auto')
+    
     return jsonify('No command sent!')
 
 @venti_bp.route('/ventiParams', methods=['POST'])
 def set_params():
     data = request.get_json()
     venti_auto_param(**data)
-    venti_control()
+    # Force scheduler to run now when params change
+    scheduler.modify_job('venti_control', next_run_time=datetime.now())
+    logger.info('Parameter aktualisiert')
     return jsonify('Parameters updated')
